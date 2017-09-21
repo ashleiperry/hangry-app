4a6137052ad55cc4e0877c614f8768072cb1b118
Ashlei Perry <ashlei.perry@gmail.com>
2017-09-21 13:34:09 -0400

    accidentally made this branch. nothing happened here

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  root 'users#index'
+  root 'static_pages#home'
+  get  'static_pages/home'
   devise_for :users
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
