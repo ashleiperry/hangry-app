54db6dc56fcd0f4de7d2d372cc149318a7acdde8
Ashlei Perry <ashlei.perry@gmail.com>
2017-09-25 15:34:19 -0400

    created user_preference table with associations and model validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,6 @@
 class Restaurant < ApplicationRecord
+  has_many :user_preferences
+  has_one :user, through: :user_preferences
   validates :name, presence: true
   validates :address, presence: true
   validates :telephone_number, presence: true
