56675716c086d8a87c3a43c4d1469b031aea6bb4
Ashlei Perry <ashlei.perry@gmail.com>
2017-09-26 10:52:01 -0400

    added join table and validations and associations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
+  has_many :order_items
+  has_many :items, through: :order_items
   validates :name, presence: true
   validates :restaurant_id, presence: true
 end
