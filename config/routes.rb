79116a90f407be37020320ead3ab21631f554797
Ashlei Perry <ashlei.perry@gmail.com>
2017-09-28 16:19:18 -0400

    added a bunch of changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   root 'orders#index'
   devise_for :users
 
-  resources :orders
+  resources :orders, except: [:new, :create]
   resources :restaurants
+  resources :restaurants, only: [:index, :show] do
+    resources :orders, only: [:index, :new, :create]
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
