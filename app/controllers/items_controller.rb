b7d15e04ca735c0aad013b63844dc84b944f36c2
Ashlei Perry <ashlei.perry@gmail.com>
2017-10-02 17:05:01 -0400

    modified css and fixed items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,8 @@
 class ItemsController < ApplicationController
   def create
-  @order = Order.find(params[:order_id])
-  binding.pry
-  @item = Item.find(params[:id])
-  OrderItem.create(@order,@item)
-  redirect_to order_path(@order)
-end
+    @order = Order.find(params[:order_id])
+    @item = Item.find(params[:format])
+    OrderItem.create(order: @order,item: @item)
+    redirect_to order_path(@order)
+  end
 end
