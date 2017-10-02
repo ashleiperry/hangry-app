d78ba25f02f63e9f43d1d4b2ab287a001324e633
Ashlei Perry <ashlei.perry@gmail.com>
2017-10-02 10:41:10 -0400

    removed quantity column from order item table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170926205500) do
+ActiveRecord::Schema.define(version: 20171002143830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(version: 20170926205500) do
     t.integer "item_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "quantity", null: false
   end
 
   create_table "orders", force: :cascade do |t|
