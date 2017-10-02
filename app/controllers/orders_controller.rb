c7891d05da9f0e499c555580425c75733aeff335
Ashlei Perry <ashlei.perry@gmail.com>
2017-10-02 14:49:58 -0400

    added new page to site

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,31 +5,37 @@ class OrdersController < ApplicationController
     @orders = Order.where(user_id: current_user)
   end
 
-  def create
-    @order = Order.new
+  def show
+      @order = Order.find(params[:id])
+      @restaurant = @order.restaurant
+      @items = @order.restaurant.items
+      @ordered_items = @order.items
+    end
+
+  def new
+      @restaurant = Restaurant.find(params[:restaurant_id])
+      @order = Order.new
   end
 
-  def show
-    @order = Order.find(params[:id])
-    @items = @order.items
-  end
+  def create
+      @restaurant = Restaurant.find(params[:restaurant_id])
+  	@order = Order.new(order_params)
+      @order.restaurant = @restaurant
+      @order.user = current_user
+
+      if @order.save
+        redirect_to orders_path, notice: "Order added successfully"
+      else
+        flash[:alert] = @order.errors.full_messages.join(' , ')
+        render :new
+      end
+    end
+
   def destroy
     @order.destroy
     redirect_to orders_path,
     notice: "Order has been successfully deleted"
   end
-  def create
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @order = Order.create(order_params)
-    @order.restaurant = @restaurant
-    if @order.save
-      redirect_to @restaurant, notice: "Order added successfully"
-    else
-      flash[:alert] = @order.errors.full_messages.join(' , ')
-      render :new
-    end
-  end
-
   private
 
   def order_params
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,5 +3,4 @@ class OrderItem < ApplicationRecord
   belongs_to :item
   validates :order_id, presence: true
   validates :item_id, presence: true
-  validates :quantity, presence: true
 end
